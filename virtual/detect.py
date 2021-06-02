64fee21302516d86afb09331bf1f37a9ada56496
asypher <asypher.ai@gmail.com>
2021-06-02 18:37:20 +0530

    mobilecheck

--- virtual/detect.py
+++ virtual/detect.py
@@ -39,12 +39,14 @@ def applyfilteronimage(image,keypoints,opacity,color,width,height,partsdic):
     img1  = cv2.cvtColor(img1,cv2.COLOR_RGB2BGR)
     keys= list(partsdic.keys())
     values = list(partsdic.values())
-    mask = np.zeros((int(height/2),int(width/2),3) ,dtype = np.uint8)
+    # mask = np.zeros((int(height/2),int(width/2),3) ,dtype = np.uint8)
+    mask = np.zeros((height,width,3) ,dtype = np.uint8)
     for i,value in enumerate(values):
         if value != "none":
             R,G,B = ImageColor.getcolor(value, "RGB")
             mask  = makepartmask(mask,keypoints ,keys[i])
-            img2  = np.full((int(height/2),int(width/2),3), (R,G,B) , dtype= np.uint8)
+            # img2  = np.full((int(height/2),int(width/2),3), (R,G,B) , dtype= np.uint8)
+            img2  = np.full((height,width,3), (R,G,B) , dtype= np.uint8)
             mask = cv2.bitwise_and(mask, img2)
 
     mask = cv2.GaussianBlur(mask, (7, 7), 10)
@@ -55,7 +57,9 @@ def applyfilteronimage(image,keypoints,opacity,color,width,height,partsdic):
             # img1[mask != 0] = img2[mask != 0] * (1-alpha) + alpha * img1[mask != 0]
     #img1 = cv2.flip(img1,1)
     # mask = cv2.flip(mask,1)
-    img1 = cv2.addWeighted(img1, 1, mask, 0.4, 0)
+    # mask = cv2.resize(mask,(width,height))
+    # logging.info("image shaep == "  + str(img1.shape) +"mask sahpe == "+ str(mask.shape))
+    img1 = cv2.addWeighted(img1, 1, mask, opacity/100, 0)
     # img1 = cv2.flip(img1,1)
     buffer = BytesIO()
     # img = Image.fromarray(mask)
